bb8b818c076d4c3e8a83588b253e9d55bff81e1c
arrowsmith001 <arrowsmithalexander@gmail.com>
2025-10-19 18:54:57 +0100

    add fonts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import preact from '@preact/preset-vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    assetsInlineLimit: 0
+  },
+  assetsInclude: ["**/*.woff2", "**/*.woff"]
 })
